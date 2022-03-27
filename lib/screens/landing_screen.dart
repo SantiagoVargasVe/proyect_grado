d40f4410f01f5c233d84361527a15bcf0e45c00e
Santiago Vargas <santiagovargasve@gmail.com>
2022-03-27 15:18:51 -0500

    ADD: Vista de la landing

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -8,13 +8,93 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? validateEmail(String? value) {
+    if (value == null) {
+      return 'El campo es requerido';
+    }
+
+    final RegExp emailExp = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+    if (!emailExp.hasMatch(value)) {
+      return 'El email no es válido';
+    }
+
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null) {
+      return 'El campo es requerido';
+    }
+
+    if (value.length < 6) {
+      return 'La contraseña debe tener al menos 6 caracteres';
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [Text('LandingScreen')],
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Image(
+              image: AssetImage('assets/images/landing_logo.png'),
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(
+              width: 250,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                    ),
+                    validator: validateEmail,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Contraseña',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                    ),
+                    validator: validateEmail,
+                  ),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('¿Olvidaste tu contraseña?'),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(300, 40),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Iniciar sesión')),
+                  TextButton(
+                      onPressed: () {}, child: const Text('Crear cuenta')),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
