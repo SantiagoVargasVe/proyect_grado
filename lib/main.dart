b9492b2c80c9d0749c9691ef901f4bbcab352674
Santiago Vargas <santiagovargasve@gmail.com>
2022-03-27 14:10:27 -0500

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/landing_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        canvasColor: const Color.fromRGBO(208, 188, 255, 1),
+      ),
+      home: LandingScreen(),
+    );
+  }
+}
---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [Text('LandingScreen')],
+      ),
+    ));
+  }
+}
