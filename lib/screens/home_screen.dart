f20d9760d4a1f079dfbd44f4aff89aec02e248b7
Santiago Vargas <santiagovargasve@gmail.com>
2022-05-05 13:16:36 -0500

    FIX: BottonNavigationBar eliminado del home, ya que soy hay comunidades

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,71 +21,71 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: _children[_selectedIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color.fromRGBO(255, 251, 254, 1),
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Ink(
-              width: 64,
-              height: 32,
-              child: const Icon(Icons.dashboard),
-              decoration: (_selectedIndex == 0)
-                  ? BoxDecoration(
-                      color: const Color.fromRGBO(232, 222, 248, 1),
-                      borderRadius: BorderRadius.circular(20))
-                  : null,
-            ),
-            label: "Talleres",
-          ),
-          BottomNavigationBarItem(
-            icon: Ink(
-              width: 64,
-              height: 32,
-              child: const Icon(Icons.school),
-              decoration: (_selectedIndex == 1)
-                  ? BoxDecoration(
-                      color: const Color.fromRGBO(232, 222, 248, 1),
-                      borderRadius: BorderRadius.circular(20))
-                  : null,
-            ),
-            label: "Comunidades",
-          ),
-          BottomNavigationBarItem(
-            icon: Ink(
-              width: 64,
-              height: 32,
-              child: const Icon(Icons.groups),
-              decoration: (_selectedIndex == 2)
-                  ? BoxDecoration(
-                      color: const Color.fromRGBO(232, 222, 248, 1),
-                      borderRadius: BorderRadius.circular(20))
-                  : null,
-            ),
-            label: "Tus talleres",
-          ),
-          BottomNavigationBarItem(
-            icon: Ink(
-              width: 64,
-              height: 32,
-              child: const Icon(Icons.perm_identity),
-              decoration: (_selectedIndex == 3)
-                  ? BoxDecoration(
-                      color: const Color.fromRGBO(232, 222, 248, 1),
-                      borderRadius: BorderRadius.circular(20))
-                  : null,
-            ),
-            label: "Perfil",
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   backgroundColor: const Color.fromRGBO(255, 251, 254, 1),
+      //   type: BottomNavigationBarType.fixed,
+      //   items: [
+      //     BottomNavigationBarItem(
+      //       icon: Ink(
+      //         width: 64,
+      //         height: 32,
+      //         child: const Icon(Icons.dashboard),
+      //         decoration: (_selectedIndex == 0)
+      //             ? BoxDecoration(
+      //                 color: const Color.fromRGBO(232, 222, 248, 1),
+      //                 borderRadius: BorderRadius.circular(20))
+      //             : null,
+      //       ),
+      //       label: "Talleres",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Ink(
+      //         width: 64,
+      //         height: 32,
+      //         child: const Icon(Icons.school),
+      //         decoration: (_selectedIndex == 1)
+      //             ? BoxDecoration(
+      //                 color: const Color.fromRGBO(232, 222, 248, 1),
+      //                 borderRadius: BorderRadius.circular(20))
+      //             : null,
+      //       ),
+      //       label: "Comunidades",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Ink(
+      //         width: 64,
+      //         height: 32,
+      //         child: const Icon(Icons.groups),
+      //         decoration: (_selectedIndex == 2)
+      //             ? BoxDecoration(
+      //                 color: const Color.fromRGBO(232, 222, 248, 1),
+      //                 borderRadius: BorderRadius.circular(20))
+      //             : null,
+      //       ),
+      //       label: "Tus talleres",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Ink(
+      //         width: 64,
+      //         height: 32,
+      //         child: const Icon(Icons.perm_identity),
+      //         decoration: (_selectedIndex == 3)
+      //             ? BoxDecoration(
+      //                 color: const Color.fromRGBO(232, 222, 248, 1),
+      //                 borderRadius: BorderRadius.circular(20))
+      //             : null,
+      //       ),
+      //       label: "Perfil",
+      //     ),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   selectedItemColor: Colors.black,
+      //   onTap: (index) {
+      //     setState(() {
+      //       _selectedIndex = index;
+      //     });
+      //   },
+      // ),
     );
   }
 }
